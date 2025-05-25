7ae42a4c0273c5a19d3ac8fa7b83a13ec66b76ce
DivaAji <diva7aji7@gmail.com>
2025-05-25 23:22:05 +0700

    Fix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // ✅ wajib
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-};
+export default nextConfig
 
-export default nextConfig;
