569c233fa94694cbdba1e89e2a761429ee179648
DivaAji <diva7aji7@gmail.com>
2025-05-26 08:29:27 +0700

    Fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   output: 'standalone',
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
